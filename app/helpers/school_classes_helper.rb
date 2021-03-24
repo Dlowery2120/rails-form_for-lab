187eab579a94bad2da8a6120f86c33c6734c003c
Devante Lowery <dlowery2120@gmail.com>
2021-03-24 11:18:32 -0500

    commit tree a4167ed65676b868bb088c2c00901ec3ada0b00b

---
+++ app/helpers/school_classes_helper.rb
@@ -0,0 +1,4 @@
+module SchoolClassesHelper
+
+    
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.destroy_all
+
+Student.create(first_name: "Devante", last_name: "Lowery")
+Student.create(first_name: "Devante", last_name: "Lowery")
+Student.create(first_name: "Devante", last_name: "Lowery")
+Student.create(first_name: "Devante", last_name: "Lowery")
+Student.create(first_name: "Devante", last_name: "Lowery")
+Student.create(first_name: "Devante", last_name: "Lowery")
+Student.create(first_name: "Devante", last_name: "Lowery")
+
+
+SchoolClass.create(title: "Homeroom", room_number: 100)
+SchoolClass.create(title: "Phase 1", room_number: 200)
+SchoolClass.create(title: "Phase 2", room_number: 150)
+SchoolClass.create(title: "Phase 3", room_number: 250)
+
+puts "created"
